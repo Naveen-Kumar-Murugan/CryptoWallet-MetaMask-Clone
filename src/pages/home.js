dd8aaee513f2e8a96bbfc4722cb686a5825c37dd
M Naveen Kumar <naveenkumar@Naveens-MacBook-Air-2.local>
2024-05-23 22:24:47 +0530

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "react-router-dom";
+export default function Home(){
+    return( 
+        <div>
+        <h1>hello</h1>
+        <Link to = "login"><h2>Login</h2></Link>
+        </div>
+    );
+}
---
+++ src/pages/send.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Send(){
+    return( 
+        <h1>send</h1>
+    );
+}
